5cae4425a3c4a465bb6b44548761750355eff586
imran <imran.shaikh@graymatrix.com>
2016-10-02 22:45:43 +0530

    files updated

--- resources/views/voice/create.blade.php
+++ resources/views/voice/create.blade.php
@@ -11,99 +11,7 @@
             </div>
             <div class="row">
             	<div class="col-lg-12">
-            		<article id="inbondForm" class="panel panel-default">
-            			<h2>Form Heading</h2>
-            			<div class="panel-body">
-            				<div class="row">
-            					<div class="col-lg-12">
-            						<!-- Form 1 -->
-            						<form>      
-            							<div class="row">
-            							<div class="col-lg-3">  							
-	            							<div class="form-group">
-	                                            <label for="transaction-id">Transaction ID</label>
-	                                            <input class="form-control" type="text" id="transactionID" placeholder="Transaction ID">
-	                                        </div>
-	                                        <div class="form-group">
-	                                            <label for="date">Date</label>
-	                                            <input class="form-control" type="text" id="date" placeholder="Date">
-	                                        </div>
-                                        </div>
-                                        </div>
-                                        <div class="form-wrapper row">
-	                                        <div class="col-lg-3">
-		                                        <div class="form-group">
-		                                            <label>Process</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label>Agent</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label>Team Leader</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label>Manager</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-	                                        </div>
-	                                        <div class="col-lg-3">
-	                                        	<div class="form-group">
-		                                            <label>Category</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label for="client-id">Client ID</label>
-                                        			<input class="form-control" type="text" id="clientID" placeholder="Client ID">
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label for="call-id">Call ID</label>
-                                        			<input class="form-control" type="text" id="callID" placeholder="Call ID">
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label for="duration">Duration</label>
-                                        			<input class="form-control" type="text" id="duration" placeholder="Duration">
-		                                        </div>
-	                                        </div>
-	                                        <div class="col-lg-3">
-	                                        	<div class="form-group">
-		                                            <label>Call Type</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-		                                        <div class="form-group">
-		                                            <label>Sub-Call Type</label>
-		                                            <select class="form-control">
-		                                                <option>1</option>
-		                                                <option>2</option>
-		                                            </select>
-		                                        </div>
-	                                        </div>
-                                        </div>
-            						</form>
-            					</div>
-            				</div>
-            			</div>
-            		</article><!-- #inbond -->
+            		@include('views.voice.templates.inbondprocess')
 
             		<article id="fatal" class="panel panel-default">            			
         				<h2>Fatal</h2>            			
@@ -454,17 +362,20 @@
 					                                        </div>
 				                                        </div>
 		                                        	</div><!-- .item -->
-												</div><!-- .row -->
-
-		                                       
-		                                        
+												</div><!-- .row -->	                                        
 	                                        </div>
-	                                        <div class="col-lg-8">
+	                                        <div class="col-lg-6">
+												<div class="form-group">
+													<label>Comments</label>
+													<textarea class="form-control" rows="3"></textarea>
+												</div>
+											</div>
+	                                        <!-- <div class="col-lg-8">
 	                                        	<div class="form-group">
 													<label>Comments</label>
 													<textarea class="form-control" rows="3"></textarea>
 												</div>
-	                                        </div>                                 
+	                                        </div>    -->                              
                                         </div>
             						</form>
             					</div>
