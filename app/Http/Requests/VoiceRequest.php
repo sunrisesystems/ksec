49d731aa78845c2e3978f2bf73fbea79d52d1ef6
Supriya <supriya.khankar@graymatrix.com>
2016-10-16 21:09:20 +0530

    Validation added

--- app/Http/Requests/VoiceRequest.php
+++ app/Http/Requests/VoiceRequest.php
@@ -3,6 +3,8 @@
 namespace ksec\Http\Requests;
 
 use ksec\Http\Requests\Request;
+use Config;
+
 
 class VoiceRequest extends Request
 {
@@ -35,6 +37,9 @@ class VoiceRequest extends Request
                 return [
                     'date' => 'required|date_format:d-M-Y',
                     'process' => 'required|exists:processes,id,status,A',
+                    'agent' => 'required|exists:employees,id,status,A,emp_type_id,'.Config::get("global_vars.HARD_CODED_ID.agent"),
+                    'teamLead' => 'required|exists:employees,id,status,A,emp_type_id,'.Config::get("global_vars.HARD_CODED_ID.teamLead"),
+                    'manager' => 'required|exists:employees,id,status,A,emp_type_id,'.Config::get("global_vars.HARD_CODED_ID.managerId"),
                   /*  'date' => 'required|unique:call_types,call_type,NULL,id,deleted_at,NULL',
                     'status' => 'required|in:A,I',
                     'description' => 'required',*/
